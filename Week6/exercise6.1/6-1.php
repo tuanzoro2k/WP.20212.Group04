b93db721bb7c8b0c2bd58d16d260cff09d9c5245
doxuandungx10 <52046541+doxuandungx10@users.noreply.github.com>
2022-06-01 23:10:17 +0700

    week 6 exercise

---
+++ Week6/exercise6.1/6-1.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+
+<head>
+    <title>8-1</title>
+    <div id="header"><title>My first styled page</title></div>
+    <link rel="stylesheet" href="8-1.css">
+</head>
+
+<body>
+    
+    <! -- Site navigation menu -->
+    <div id="navbar">
+        <ul class="navbar">
+            <li><a href="index.html">Home page</a>
+            <li><a href="musings.html">Musings</a>
+            <li><a href="town.html">My town</a>
+            <li><a href="links.html">Links</a>
+        </ul>
+    </div>
+        <div id="content">
+        <! -- Main content -->
+            <h1>My first styled page</h1>
+            <p>Welcome to my styled page!
+            <p>It lacks images, but at least it has style.And it has links, even if they don't go anywhere & hellip;
+            <p>There should be more here, but I don't know what yet.
+            <! -- Sign and date the page, it's only polite! -->
+            <div id="address"><address>Made 15 October 2009<br>
+            by myself.</address></div>
+        </div>
+</body>
+
+</html>
---
+++ Week6/exercise6.2/6-2.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>8-2</title>
+    <link rel="stylesheet" href="8-2.css">
+</head>
+
+<body>
+	<header>
+		<nav>  
+			<ul>
+				<li><a href="#">Home</a>
+				<li><a href="#">Notice</a>
+				<li><a href="#">Message</a>
+				<li><a href="#">Log out</a>
+			</ul>
+		</nav>
+	</header>
+	<section>
+		<strong>Demo</strong>
+	</section>
+	<section id="pageContent">
+		<main role="main">
+			<article>
+				<h2>HTML</h2>
+				<p>Hyper Text Markup Language (HTML) is a markup language[1] for creating a webpage. Webpages are usually viewed in a web browser. They can include writing, links, pictures, and even sound and video. HTML is used to mark and describe each of these kinds of content so the web browser can display them correctly. HTML can also be used to add meta information to a webpage. Meta information is usually not shown by web browsers and is data about the web page, e.g., the name of the person who created the page. Cascading Style Sheets (CSS) is used to style HTML elements while JavaScript is used to manipulate HTML elements and CSS styles.</p>
+			</article>
+			<article>
+				<h2>CSS</h2>
+				<p>Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language like HTML.[1] CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.</p>
+			</article>
+		</main>
+		<aside>
+			<div>This is sidebar 1</div>
+			<div>This is sidebar 2</div>
+			<div>This is sidebar 3</div>
+		</aside>
+	</section>
+	<footer>
+		<p>This is footer</p>
+	</footer>
+
+
+</body>
+
+</html>
