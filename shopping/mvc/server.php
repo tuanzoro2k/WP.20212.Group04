c28b812ecf929739e82f004452f4b875ee9e5f0f
tuannguyen2k5 <nguyentientuan20052000@gmail.com>
2022-07-22 23:38:22 +0700

    modify path router

--- shopping/mvc/server.php
+++ shopping/mvc/server.php
@@ -1,8 +1,8 @@
 <?php
- require_once "./mvc/core/App.php";
- require_once "./mvc/core/controller.php";
- require_once "./mvc/core/db.php";
- require_once "./mvc/core/constant.php";
- require_once "./mvc/helper/JWTOKEN.php";
- require_once "./mvc/config/Router.php";
- require_once "./mvc/config/Routes.php";
+require_once "./mvc/core/App.php";
+require_once "./mvc/core/controller.php";
+require_once "./mvc/core/db.php";
+require_once "./mvc/core/constant.php";
+require_once "./mvc/helper/JWTOKEN.php";
+require_once "./mvc/router/Router.php";
+require_once "./mvc/router/Routes.php";
